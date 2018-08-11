57e8e188a5911a3cbe9825027ec2081ec0cd4383
Benard18 <benkaranja43@gmail.com>
2018-08-11 16:35:55 +0300

    adding the message form

--- art/forms.py
+++ art/forms.py
@@ -1,3 +1,8 @@
 from django.contrib.auth.models import User
 from django import forms
-from .models import *
+from .models import *from .models import *
+
+class Dmform(forms.ModelForm):
+	class Meta:
+		model = Messages
+		exclude = ['sender', 'reciever', 'time_sent', 'read']
