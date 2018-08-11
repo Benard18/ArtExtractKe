66c1bc4eddeb605000f6b223bf9866808192f233
Benard18 <benkaranja43@gmail.com>
2018-08-11 16:36:24 +0300

    adding the profile and post forms

--- art/forms.py
+++ art/forms.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import User
 from django import forms
 from .models import *from .models import *
+from .models import *
 
 class Dmform(forms.ModelForm):
 	class Meta:
 		model = Messages
 		exclude = ['sender', 'reciever', 'time_sent', 'read']
+
+class NewProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		exclude = ['user'] 	
+		
+class NewPostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		exclude = ['user']
