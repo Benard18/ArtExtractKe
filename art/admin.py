88e4347afc5e48963a500738feb9d35ba0062e65
Benard18 <benkaranja43@gmail.com>
2018-08-11 16:32:19 +0300

    adding the model blueprints in the admin dashboard

--- art/admin.py
+++ art/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Company)
+admin.site.register(Messages)
+admin.site.register(Comments)
+admin.site.register(Categories)
+admin.site.register(Post)
